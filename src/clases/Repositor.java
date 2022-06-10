a5840d39b2559e2559b32f9b76e082632cae62b4
Ttbias2 <tobiasvalentini73@gmail.com>
2022-06-09 23:25:52 -0300

    0.01 clases y metodos iniciales

---
+++ src/clases/Repositor.java
@@ -0,0 +1,19 @@
+package clases;
+
+public class Repositor extends Empleado{
+
+	public Repositor(String nombre, String apellido, int edad, String dni, String nombreDeUsuario, String contraseña,
+			float sueldo, String horario, int horasTrabajadas, int horasExtras,float pagoPorHoras) {
+		
+		super(nombre, apellido, edad, dni, nombreDeUsuario, contraseña, sueldo, horario, pagoPorHoras);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+	
+
+}
